f63e8994e5503d5da53fc734235c91c083995813
DemonWav <demonwav@gmail.com>
2013-12-05 09:19:51 -0600

    Changed the reset stats command to run on a second thread, I am working on making everything that writes to disk run in a separate thread. I am not sure if this implimentation will work, though, as I am not sure if runTAskAsynchronously waits for the task to finish before proceeding.

--- wav/demon/Commands/ResetCommand.java
+++ wav/demon/Commands/ResetCommand.java
@@ -16,149 +16,48 @@ import java.util.Map;
 public class ResetCommand implements CommandExecutor {
 
     private StatCraft plugin;
+    public boolean result;
 
     public ResetCommand(StatCraft plugin) {
         this.plugin = plugin;
     }
 
     @Override
-    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+    public boolean onCommand(final CommandSender commandSender, Command command, String s, final String[] strings) {
         if (strings.length > 3)
             return false;
         else if (strings[0].equalsIgnoreCase("force-all") && strings.length == 1) {
-            if (commandSender.hasPermission("admin.*")) {
-                File statsDir = new File(plugin.getDataFolder(), "stats");
-                try {
-                    deleteRecursive(statsDir);
-                } catch (FileNotFoundException e) {
-                    System.out.println("StatCraft: Fatal Error occurred while trying to delete stats.");
-                    e.printStackTrace();
+            plugin.getServer().getScheduler().runTaskAsynchronously(plugin, new Runnable() {
+                @Override
+                public void run() {
+                    result = resetAll(commandSender);
                 }
-                plugin.statsForPlayers = new HashMap<String, Map<Integer, Map<String, Integer>>>();
-                for (Player player :commandSender.getServer().getOnlinePlayers()) {
-                    plugin.playtime.addStatToPlayer(StatTypes.LAST_JOIN_TIME.id, player.getName(),
-                            (int) (System.currentTimeMillis() / 1000));
-                }
-                return true;
-            }
-            commandSender.sendMessage("You don't have permission to do that.");
-            return false;
+            });
+            return result;
         } else if (strings[0].equalsIgnoreCase("force-all") && strings.length == 2) {
-            if (commandSender.hasPermission("admin.*")) {
-                plugin.saveStatFiles();
-                String type = strings[1];
-
-                for (StatTypes stat : StatTypes.values())
-                    if (type.equalsIgnoreCase(stat.title))
-                        type = stat.id + "";
-
-                File statsDir = new File(plugin.getDataFolder(), "stats");
-                try {
-                    deleteType(statsDir, type);
-                    plugin.reloadStatFiles();
-                } catch (FileNotFoundException e) {
-                    System.out.println("StatCraft: Fatal Error occurred while trying to delete stats.");
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    System.out.println("StatCraft: Fatal Error occurred while trying to reload stats.");
-                    e.printStackTrace();
+            plugin.getServer().getScheduler().runTaskAsynchronously(plugin, new Runnable() {
+                @Override
+                public void run() {
+                    result = resetAllStat(commandSender, strings);
                 }
-                for (Player player : commandSender.getServer().getOnlinePlayers()) {
-                    plugin.playtime.addStatToPlayer(StatTypes.LAST_JOIN_TIME.id, player.getName(),
-                            (int) (System.currentTimeMillis() / 1000));
-                }
-                return true;
-            }
-            commandSender.sendMessage("You don't have permission to do that.");
-            return false;
+            });
+            return result;
         } else if (strings[0].equalsIgnoreCase("force") && strings.length == 2) {
-            String name = strings[1];
-            if (name.equalsIgnoreCase(commandSender.getName())) {
-                File statsDir = new File(plugin.getDataFolder(), "stats/" + name);
-                try {
-                    deleteRecursive(statsDir);
-                } catch (FileNotFoundException e) {
-                    System.out.println("Fatal Error occurred while trying to delete " + name + "'s stats.");
-                    e.printStackTrace();
+            plugin.getServer().getScheduler().runTaskAsynchronously(plugin, new Runnable() {
+                @Override
+                public void run() {
+                    result = resetAllUser(commandSender, strings);
                 }
-                if (plugin.statsForPlayers.containsKey(name))
-                    plugin.statsForPlayers.put(name, new HashMap<Integer, Map<String, Integer>>());
-
-                plugin.playtime.addStatToPlayer(StatTypes.LAST_JOIN_TIME.id, name,
-                        (int) (System.currentTimeMillis() / 1000));
-                return true;
-            } else if (commandSender.hasPermission("admin.*")) {
-                File statsDir = new File(plugin.getDataFolder(), "stats/" + name);
-                try {
-                    deleteRecursive(statsDir);
-                } catch (FileNotFoundException e) {
-                    System.out.println("Fatal Error occurred while trying to delete " + name + "'s stats.");
-                    e.printStackTrace();
-                }
-                if (plugin.statsForPlayers.containsKey(name))
-                    plugin.statsForPlayers.put(name, new HashMap<Integer, Map<String, Integer>>());
-
-                plugin.playtime.addStatToPlayer(StatTypes.LAST_JOIN_TIME.id, name,
-                        (int) (System.currentTimeMillis() / 1000));
-                return true;
-            } else {
-                commandSender.sendMessage("You don't have permission to do that.");
-            }
-            return false;
+            });
+            return result;
         } else if (strings[0].equalsIgnoreCase("force") && strings.length == 3) {
-            String name = strings[1];
-            String typeString = strings[2];
-
-            for (StatTypes stat : StatTypes.values())
-                if (typeString.equalsIgnoreCase(stat.title))
-                    typeString = stat.id + "";
-
-            int type;
-            try {
-                type = Integer.parseInt(typeString);
-            } catch (NumberFormatException e) {
-                return false;
-            }
-            if (name.equalsIgnoreCase(commandSender.getName())) {
-                File statsDir = new File(plugin.getDataFolder(), "stats/" + name);
-                try {
-                    deleteType(statsDir, typeString);
-                } catch (FileNotFoundException e) {
-                    System.out.println("Fatal Error occurred while trying to delete " + name + "'s stats.");
-                    e.printStackTrace();
+            plugin.getServer().getScheduler().runTaskAsynchronously(plugin, new Runnable() {
+                @Override
+                public void run() {
+                    result = resetAllUserStat(commandSender, strings);
                 }
-
-                if (plugin.statsForPlayers.containsKey(name))
-                    if (plugin.statsForPlayers.get(name).containsKey(type))
-                        plugin.statsForPlayers.get(name).put(type, new HashMap<String, Integer>());
-
-                if (type == StatTypes.LAST_JOIN_TIME.id)
-                    plugin.playtime.addStatToPlayer(StatTypes.LAST_JOIN_TIME.id, name,
-                            (int) (System.currentTimeMillis() / 1000));
-
-                return true;
-            } else if (commandSender.hasPermission("admin.*")) {
-                File statsDir = new File(plugin.getDataFolder(), "stats/" + name);
-                try {
-                    deleteType(statsDir, typeString);
-                } catch (FileNotFoundException e) {
-                    System.out.println("Fatal Error occurred while trying to delete " + name + "'s stats.");
-                    e.printStackTrace();
-                }
-
-                if (plugin.statsForPlayers.containsKey(name))
-                    if (plugin.statsForPlayers.get(name).containsKey(type))
-                        plugin.statsForPlayers.get(name).put(type, new HashMap<String, Integer>());
-
-                if (type == StatTypes.LAST_JOIN_TIME.id)
-                    plugin.playtime.addStatToPlayer(StatTypes.LAST_JOIN_TIME.id, name,
-                            (int) (System.currentTimeMillis() / 1000));
-
-                return true;
-            } else {
-                commandSender.sendMessage("You don't have permission to do that.");
-            }
-            return false;
+            });
+            return result;
         } else {
             return false;
         }
@@ -187,5 +86,153 @@ public class ResetCommand implements CommandExecutor {
             return ret && path.delete();
         else
             return ret;
+    }
+
+    private synchronized boolean resetAll(CommandSender commandSender) {
+        if (commandSender.hasPermission("admin.*")) {
+            File statsDir = new File(plugin.getDataFolder(), "stats");
+            try {
+                deleteRecursive(statsDir);
+            } catch (FileNotFoundException e) {
+                System.out.println("StatCraft: Fatal Error occurred while trying to delete stats.");
+                e.printStackTrace();
+            }
+            plugin.statsForPlayers = new HashMap<String, Map<Integer, Map<String, Integer>>>();
+            for (Player player :commandSender.getServer().getOnlinePlayers()) {
+                plugin.playtime.addStatToPlayer(StatTypes.LAST_JOIN_TIME.id, player.getName(),
+                        (int) (System.currentTimeMillis() / 1000));
+            }
+            return true;
+        }
+        commandSender.sendMessage("You don't have permission to do that.");
+        return false;
+    }
+
+    private synchronized boolean resetAllStat(CommandSender commandSender, String[] strings) {
+        if (commandSender.hasPermission("admin.*")) {
+            plugin.saveStatFiles();
+            String type = strings[1];
+
+            for (StatTypes stat : StatTypes.values())
+                if (type.equalsIgnoreCase(stat.title))
+                    type = stat.id + "";
+
+            File statsDir = new File(plugin.getDataFolder(), "stats");
+            try {
+                deleteType(statsDir, type);
+                plugin.reloadStatFiles();
+            } catch (FileNotFoundException e) {
+                System.out.println("StatCraft: Fatal Error occurred while trying to delete stats.");
+                e.printStackTrace();
+                return false;
+            } catch (IOException e) {
+                System.out.println("StatCraft: Fatal Error occurred while trying to reload stats.");
+                e.printStackTrace();
+                return false;
+            }
+            for (Player player : commandSender.getServer().getOnlinePlayers()) {
+                plugin.playtime.addStatToPlayer(StatTypes.LAST_JOIN_TIME.id, player.getName(),
+                        (int) (System.currentTimeMillis() / 1000));
+            }
+            return true;
+        }
+        commandSender.sendMessage("You don't have permission to do that.");
+        return false;
+    }
+
+    private synchronized boolean resetAllUser(CommandSender commandSender, String[] strings) {
+        String name = strings[1];
+        if (name.equalsIgnoreCase(commandSender.getName())) {
+            File statsDir = new File(plugin.getDataFolder(), "stats/" + name);
+            try {
+                deleteRecursive(statsDir);
+            } catch (FileNotFoundException e) {
+                System.out.println("Fatal Error occurred while trying to delete " + name + "'s stats.");
+                e.printStackTrace();
+            }
+            if (plugin.statsForPlayers.containsKey(name))
+                plugin.statsForPlayers.put(name, new HashMap<Integer, Map<String, Integer>>());
+
+            plugin.playtime.addStatToPlayer(StatTypes.LAST_JOIN_TIME.id, name,
+                    (int) (System.currentTimeMillis() / 1000));
+            return true;
+        } else if (commandSender.hasPermission("admin.*")) {
+            File statsDir = new File(plugin.getDataFolder(), "stats/" + name);
+            try {
+                deleteRecursive(statsDir);
+            } catch (FileNotFoundException e) {
+                System.out.println("Fatal Error occurred while trying to delete " + name + "'s stats.");
+                e.printStackTrace();
+                return false;
+            }
+            if (plugin.statsForPlayers.containsKey(name))
+                plugin.statsForPlayers.put(name, new HashMap<Integer, Map<String, Integer>>());
+
+            plugin.playtime.addStatToPlayer(StatTypes.LAST_JOIN_TIME.id, name,
+                    (int) (System.currentTimeMillis() / 1000));
+            return true;
+        } else {
+            commandSender.sendMessage("You don't have permission to do that.");
+        }
+        return false;
+    }
+
+    private synchronized boolean resetAllUserStat(CommandSender commandSender, String[] strings) {
+        String name = strings[1];
+        String typeString = strings[2];
+
+        for (StatTypes stat : StatTypes.values())
+            if (typeString.equalsIgnoreCase(stat.title))
+                typeString = stat.id + "";
+
+        int type;
+        try {
+            type = Integer.parseInt(typeString);
+        } catch (NumberFormatException e) {
+            commandSender.sendMessage("That is not a valid stat type.");
+            return false;
+        }
+        if (name.equalsIgnoreCase(commandSender.getName())) {
+            File statsDir = new File(plugin.getDataFolder(), "stats/" + name);
+            try {
+                deleteType(statsDir, typeString);
+            } catch (FileNotFoundException e) {
+                System.out.println("Fatal Error occurred while trying to delete " + name + "'s stats.");
+                e.printStackTrace();
+                return false;
+            }
+
+            if (plugin.statsForPlayers.containsKey(name))
+                if (plugin.statsForPlayers.get(name).containsKey(type))
+                    plugin.statsForPlayers.get(name).put(type, new HashMap<String, Integer>());
+
+            if (type == StatTypes.LAST_JOIN_TIME.id)
+                plugin.playtime.addStatToPlayer(StatTypes.LAST_JOIN_TIME.id, name,
+                        (int) (System.currentTimeMillis() / 1000));
+
+            return true;
+        } else if (commandSender.hasPermission("admin.*")) {
+            File statsDir = new File(plugin.getDataFolder(), "stats/" + name);
+            try {
+                deleteType(statsDir, typeString);
+            } catch (FileNotFoundException e) {
+                System.out.println("Fatal Error occurred while trying to delete " + name + "'s stats.");
+                e.printStackTrace();
+                return false;
+            }
+
+            if (plugin.statsForPlayers.containsKey(name))
+                if (plugin.statsForPlayers.get(name).containsKey(type))
+                    plugin.statsForPlayers.get(name).put(type, new HashMap<String, Integer>());
+
+            if (type == StatTypes.LAST_JOIN_TIME.id)
+                plugin.playtime.addStatToPlayer(StatTypes.LAST_JOIN_TIME.id, name,
+                        (int) (System.currentTimeMillis() / 1000));
+
+            return true;
+        } else {
+            commandSender.sendMessage("You don't have permission to do that.");
+        }
+        return false;
     }
 }
