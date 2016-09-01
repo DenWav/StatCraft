6699fe197cbf877943bcfd56cb19a77ab1922614
DemonWav <demonwav@gmail.com>
2016-08-31 20:23:24 -0500

    StatCraftBean

---
+++ src/main/java/com/demonwav/statcraft/querydsl/StatCraftBean.java
@@ -0,0 +1,13 @@
+/*
+ * StatCraft Bukkit Plugin
+ *
+ * Copyright (c) 2016 Kyle Wood (DemonWav)
+ * https://www.demonwav.com
+ *
+ * MIT License
+ */
+
+package com.demonwav.statcraft.querydsl;
+
+public interface StatCraftBean {
+}
